f17bf4947299d56da16db11dd2cc2a93e674d91f
dp <736090191@qq.com>
2020-02-19 14:32:09 +0800

    update

--- run_case.py
+++ run_case.py
@@ -1,12 +1,4 @@
-"""
-------------------------------------
-@Time : 2019/6/1 14:26
-@Auth : linux超
-@File : run_case.py
-@IDE  : PyCharm
-@Motto: Real warriors,dare to face the bleak warning,dare to face the incisive error!
-------------------------------------
-"""
+# coding=utf-8
 import os
 import unittest
 
